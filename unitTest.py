43c9d757946caa9861706ec511838aec130d1835
NewFate <ronald33838@yahoo.ca>
2016-10-06 23:10:42 -0400

    Changed unit test cases

--- unitTest.py
+++ unitTest.py
@@ -6,10 +6,10 @@ class TestCrawler2(unittest.TestCase):
 
 	# the two test outputs given in the handout
     def test_print_inverted_index(self):
-        self.assertEqual(print_inverted_index(), "{ '1': set([1, 2]), '2': set([1, 3])")
+        self.assertEqual(print_inverted_index(), "{ '1': set([1]), '2': set([1, 2, 3]), '3': set([1]), '4': set([1, 3]), '5': set([1, 3]), '6': set([1, 3]) }, '7': set([2]) }, '8': set([2]) }, '9': set([2]) }")
 
     def test_print_resolved_inverted_index(self):
-        self.assertEqual(print_resolved_inverted_index(), "{ 'google': set(['http://google.ca', 'http://google.ca']), 'search': set(['http://google.ca', 'http://bing.com	'])")
+        self.assertEqual(print_resolved_inverted_index(), "{ 'words': set(['Yahoo']), 'search': set(['Yahoo', 'google', 'bing'])}")
 
 if __name__ == '__main__':
     unittest.main()
